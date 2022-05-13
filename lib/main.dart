3b65c8055da4d7d73c92a841861b37e6ffeec9ef
Saw Aung Aung <sawaungaung@BIMs-MacBook-Pro.local>
2022-05-13 11:13:23 +0630

    add hello world

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        //this is added by kk hello world
         primarySwatch: Colors.blue,
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
