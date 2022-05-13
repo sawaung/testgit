b13a2689afcb306cf7c2d89e9c76ce8caf884030
Saw Aung Aung <sawaungaung@BIMs-MacBook-Pro.local>
2022-05-13 11:46:55 +0630

    add new

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         //this is added by kk hello world
+        //this is new added by kkq
 
         primarySwatch: Colors.blue,
       ),
